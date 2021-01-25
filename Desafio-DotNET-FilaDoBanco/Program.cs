93410895fde65ea9037b717169e0ce10591a696b
F7Villani <felipevillani.2000@hotmail.com>
2021-01-25 13:07:23 -0300

    Inicialização do Desafio Fila do Banco

--- Desafio-DotNET-FilaDoBanco/Program.cs
+++ Desafio-DotNET-FilaDoBanco/Program.cs
@@ -6,7 +6,23 @@ namespace Desafio_DotNET_FilaDoBanco
     {
         static void Main()
         {
-            int N = int32.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+            for(int i = 0; i<N; i++)
+            {
+                int numeroClientes = int.Parse(Console.ReadLine());
+                int[] senhas =  new int[numeroClientes];
+                string[] senhasString = Console.ReadLine().Split(" ");
+                for(int j = 0; j<numeroClientes; j++)
+                {
+                    senhas[j] = int.Parse(senhasString[j]);
+                }
+                int[] senhasOrdenadas = new int[numeroClientes];
+                senhasOrdenadas = senhas;
+                Array.Sort(senhasOrdenadas);
+                
+                
+
+            }
         }
     }
 }
